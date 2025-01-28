cdf0f81b1850e738fbf938933ccb4dc13b0c1afe
Anbazhagan R <147583133+ANBU5994@users.noreply.github.com>
2025-01-28 20:46:48 +0530

    Add files via upload

---
+++ Baaic Problem Statement - 4.py
@@ -0,0 +1,17 @@
+#You have three hidden numbers, and your mission is to find out which one is the 
+#greatest. Write a program that can reveal the highest of these three numbers.
+#8A. Perform the above operation using ternary operator.
+
+a=int(input("enter the number 1"))
+b=int(input("enter the number 2"))
+c=int(input("enter the number 3"))
+
+if a>=b and a>c:
+    print("a is bigger")
+    
+elif b>c and b>a:
+    print("b is bigger") 
+else:
+    print("c is bigger")
+
+
