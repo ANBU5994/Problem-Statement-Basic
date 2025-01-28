f234e200c86e8c2a9fd8be0b99e736125faac68f
Anbazhagan R <147583133+ANBU5994@users.noreply.github.com>
2025-01-28 22:31:21 +0530

    Baaic Problem Statement -3.py

--- Baaic Problem Statement -3.py
+++ Baaic Problem Statement -3.py
@@ -1,14 +1,14 @@
-#Imagine you have two secret letters, „A‟ and „B‟. Your task is to write a program that 
-#uses different comparison tools to uncover how these letters relate to each other. Can you 
-#figure out which one is greater or less than the other? Use your program to solve this 
-#letter comparison mystery!
-#For example, consider two characters „A‟ and „B‟. 
-#„A‟ < „B‟ is true.
-#„A‟ <= „B‟ is true.
-#„A‟ > „B‟ is false. 
-#„A‟ >= „B‟ is false. 
-#„A‟ == „B‟ is false. 
-#„A‟ != „B‟ is true.
+'''Imagine you have two secret letters, „A‟ and „B‟. Your task is to write a program that 
+uses different comparison tools to uncover how these letters relate to each other. Can you 
+figure out which one is greater or less than the other? Use your program to solve this 
+letter comparison mystery!
+For example, consider two characters „A‟ and „B‟. 
+„A‟ < „B‟ is true.
+„A‟ <= „B‟ is true.
+„A‟ > „B‟ is false. 
+„A‟ >= „B‟ is false. 
+„A‟ == „B‟ is false. 
+„A‟ != „B‟ is true.'''
 
 
 a=input("enter the string A")
