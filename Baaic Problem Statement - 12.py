fbc8d2fc38df9aff8b7a655bd04d5a42d1b2b21e
Anbazhagan R <147583133+ANBU5994@users.noreply.github.com>
2025-01-30 19:50:40 +0530

    Add files via upload

---
+++ Baaic Problem Statement - 12.py
@@ -0,0 +1,9 @@
+'''Imagine you need to repeat a cheerful message. Write a program that uses a for 
+loop to print "ALL IS WELL" exactly twenty times. How will you set up your loop to 
+ensure this message appears the right number of times?'''
+
+
+def all(n):
+    for i in range(1,21,1):
+        print("all is well")
+print(all(n)) 
