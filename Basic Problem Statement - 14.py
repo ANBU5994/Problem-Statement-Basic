5e2ee99b5197e1e803d9b4709495baa9e64adcf2
Anbazhagan R <147583133+ANBU5994@users.noreply.github.com>
2025-02-05 22:03:34 +0530

    Add files via upload

---
+++ Basic Problem Statement - 14.py
@@ -0,0 +1,26 @@
+'''Today coding questions are
+Diamond star Pattern/ Sandglass Star Pattern
+ *
+ ***
+ *****
+ *******
+ *********
+ ***********
+************
+ ***********
+ *********
+ *******
+ *****
+ ***
+ *'''
+
+``
+n=5 
+for i in range(n):
+    for j in range(i):
+        print("*",end=" ")
+    print("* ")
+for i in range(n):
+    for j in range(i,n-1):
+        print("*",end=" ")
+    print("* ")
