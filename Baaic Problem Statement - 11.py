0aaf2711621ed75fb2c0d4ca45a397acce0fe143
Anbazhagan R <147583133+ANBU5994@users.noreply.github.com>
2025-03-08 22:33:42 +0530

    Basic Problem Statement - 11.py

--- Baaic Problem Statement - 11.py
+++ Baaic Problem Statement - 11.py
@@ -1,6 +1,7 @@
 '''You have a number to examine, and your mission is to write a program that checks 
 whether this number can be divided evenly by 27. Can you find out if it‟s a multiple of 
 27?'''
+
 def num(n):
     if (n %27==0):
         for i in range(1,11,1):
